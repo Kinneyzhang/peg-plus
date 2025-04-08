c1375a216d0f74f079c493ddb703738821e1d953
Kinneyzhang <zhangkai18@asiainfo.com>
2025-04-08 18:03:25 +0800

    update

--- peg-plus.el
+++ peg-plus.el
@@ -1,12 +1,13 @@
 ;; -*- lexical-binding: t -*-
 
 (require 'peg)
-;; (require 'peg-macros)
 (require 'peg-utils)
 
 (with-eval-after-load 'peg
   (put 'define-peg-rule 'lisp-indent-function 'defun)
   (put 'define-peg-ruleset 'lisp-indent-function 'defun))
+
+;;; peg group
 
 (defvar peg-group-data nil
   "A plist data returned by last peg-run+.")
@@ -86,11 +87,6 @@ it in group."
       ;; specific group data
       (nth 2 data))))
 
-;; (defun peg-group-check-rule ()
-;;   "检查在同一个 peg-run 的规则里，group 参数是否全部设置 prop
-;; 全部不设置。不允许部分设置 keyword。"
-;;   )
-
 (define-peg-rule group (pex &optional prop)
   (and `(-- (point)) (funcall pex) `(-- (point))
        `(start end --
@@ -104,143 +100,73 @@ it in group."
                                  ;; cons list
                                  (list data))))))))
 
-;; (cl-defmethod peg--translate ((_ (eql any)) )
-;;   '(when (not (eolp))
-;;      (forward-char)
-;;      t))
+;;; peg group ends here
 
-(peg--translate 'any)
-(peg--translate 'and '(str "emacs") '(str "vim"))
-(peg-translate-exp '(and (str "emacs") (str "vim")))
-
-peg-leaf-types
-
-(t-ps (and (+ (any)) (if "emacs"))
-      "happy hacking emacs")
-
-(peg-run
- (save-excursion
-   (or (and (or (when (looking-at '"emacs")
-                  (goto-char (match-end 0)) t)
-                (peg--record-failure '(str "emacs")))
-            (or (when (looking-at '"vim")
-                  (goto-char (match-end 0)) t)
-                (peg--record-failure '(str "vim"))))
-       (peg--record-failure '(and (str "emacs") (str "vim"))))))
-
-emacsvim
-
-(peg-run (peg (+ (any)) "emacs"))
-
-(with-temp-buffer
-  ;; (insert "happy hacking emacs and vscode!")
-  (insert "happy hacking vim and vscode!
-happy hacking emacs and vscode!")
-  (goto-char (point-min))
-  (peg-run (peg (before (peg "emacs"))))
-  ;; (point)
-  )
-
-(define-peg-rule before (pex)
-  ;; 1.当前光标位置为 pex，不移动光标，匹配成功
-  ;; 2.光标后没有匹配的 pex, 不移动光标，匹配失败
-  ;; 3.光标后面有匹配的 pex, 移动光标到 pex之前，匹配成功
-  ;; 需要一个规则判断后续的是否有匹配的，但不移动光标
-  (or
-   (has (funcall pex)) ;; extend to if, do not move cursor if failed
-   (if (funcall pex))
-   (and (+ (and (not (funcall pex)) (any)))
-        (not (eob)))
-   ;; if pex is (eob)
-   (and (+ (and (not (funcall pex)) (any)))
-        (eob) (funcall pex))))
-
-(peg-run (peg (before (peg "emacs"))))
-(peg-run (peg (if (and (* (not "emacs") (any))
-                       "emacs"))))
-
-(peg-run (peg (if (+ (and (not "emacs") (any))))))
-
-(peg-run (peg (if (and (+ (and (not "emacs") (any)))
-                       (if "emacs")))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defmacro t-ps (pex string)
-  `(with-temp-buffer
-     (save-excursion (insert ,string))
-     (peg-run (peg (and (bob) ,pex (eob))))))
-
-(define-peg-rule Qu (rule m n)
+(define-peg-rule loop (pex n)
+  ;; match N pex
   (guard
    (let ((cnt 0))
-     (while (and (< cnt n) (funcall rule))
-       (cl-incf cnt))
-     (if (<= m cnt n) t nil))))
-
-(t-ps (Qu (peg " ") 3 5) "      ") ;;=> nil
-(t-ps (Qu (peg " ") 1 2) " ") ;;=> t
-(t-ps (Qu (peg " ") 1 2) "  ") ;;=> t
-(t-ps (Qu (peg " ") 1 2) "   ") ;;=> nil
-
-(define-peg-rule loop (rule n)
-  (guard
-   (let ((cnt 0))
-     (while (and (< cnt n) (funcall rule))
+     (while (and (< cnt n) (funcall pex))
        (cl-incf cnt))
      (if (>= cnt n) t nil))))
 
-(peg-run (peg (loop (peg "emacs") 2)))
+(define-peg-rule between (pex m n)
+  ;; match the number of pex between M and N.
+  (guard
+   (let ((cnt 0))
+     (while (and (< cnt n) (funcall pex))
+       (cl-incf cnt))
+     (if (<= m cnt n) t nil))))
 
-emacsemacsemacsemacs
-
-;; (cl-defmethod peg--translate ((_ (eql   any)) &optional n)
-;;   `(when (not (eobp))
-;;      (forward-char ,(or n 1))
-;;      t))
-
-(define-peg-rule group-before (pex &optional prop)
-  ;; match any chars before pex and group.
-  (group (peg (before pex)) prop))
+(define-peg-rule before (pex)
+  ;; match any chars before the start of PEX.
+  (and (* (and (not (funcall pex)) (any)))        
+       (if (funcall pex))))
 
 (define-peg-rule until (pex)
   ;; match any chars until the end of PEX.
   (before pex) (funcall pex))
 
-(define-peg-rule before-point (pex pos)
-  ;; search PEX before point POS.
-  (until pex) (to pos))
+(define-peg-rule match (pex)
+  (before pex) (group pex))
 
-(define-peg-rule to (pos)
+(define-peg-rule any-to (pos)
   ;; match any chars to point POS.
-  (or (+ (and (guard (< (point) pos)) (any)))
-      (and (guard (= (point) pos)))))
+  (* (and (guard (< (point) pos)) (any)))
+  (guard (= (point) pos)))
 
-(define-peg-rule group-until (pex &optional prop)
-  ;; match any chars before pex and group.
-  (group (peg (until pex)) prop))
+(defmacro peg-string (string pex)
+  "Return if STRING match PEX."
+  `(with-temp-buffer
+     (save-excursion (insert ,string))
+     (peg-run (peg (and (bob) ,pex (eob))))))
 
-(define-peg-rule group-pex (pex &optional prop)
-  ;; 从当前位置开始匹配，并捕获 pex
-  (before pex) (group pex prop))
+(defmacro peg-run+ (&rest pexs)
+  "Do not move cursor if search failed."
+  `(let ((start (point)))
+     (if (peg-run (peg ,@pexs))
+         t
+       (goto-char start)
+       nil)))
 
-(define-peg-rule group-pex-to (pex pos &optional prop)
-  ;; 从当前位置匹配所有 PEX 到 POS 位置为止
-  (+ (and (group-pex pex prop)
-          (guard (< (point) pos)))))
+(defmacro peg-search-forward (pex)
+  "Search PEX in current buffer and return all matched data."
+  `(peg-run+ (match (peg ,pex))))
 
-(define-peg-rule group-to (pos &optional prop)
-  ;; 匹配任意字符到 pos 位置为止
-  (group (peg (to pos)) prop))
+(defmacro peg-search (pex &optional limit)
+  (let ((limit (or limit (point-max))))
+    (while (< (point) limit)
+      (peg-search-forward pex))))
 
-(define-peg-rule n (point n)
-  ;; match N times any chars, POINT must be (point)
-  (* (and (guard (< (point) (+ point n))) (any))))
+;; (progn
+;;   (peg-search "emacs" 5287)
+;;   (peg-group-string 1))
 
-(defun peg-eval (&optional pex)
-  (interactive "sInput PEX: ")
-  (eval-expression
-   `(peg-run (peg ,(car (read-from-string pex))))))
+;; (setq peg-group-data nil)
+
+;; (peg-run+ (match (peg "emacs")))
 
 (provide 'peg-plus)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; emacs
+;; vim
+;; vscode
